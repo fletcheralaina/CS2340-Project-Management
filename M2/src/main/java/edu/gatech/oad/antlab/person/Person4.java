d0a4da409cd3403cb260717a0b23161feb3f1b38
Safa <ssquadri117@gmail.com>
2017-09-08 09:10:05 -0400

    Person file updates (1)

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -4,8 +4,8 @@ package edu.gatech.oad.antlab.person;
  *  A simple class for person 4
  *  returns their name and a
  *  modified string 
- *  
- *  @author Bob
+ *
+ *  @author Safa
  *  @version 1.1
  */
 public class Person4 {
@@ -30,8 +30,8 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      //Person 4 put your implementation her
+      return input + 'a';
     }
     
     /**
