fdc7adabf11c3c03a96aeabc0d6c43472e2520f7
Joshua Perry <joshuacperry95@gmail.com>
2017-09-08 23:44:10 -0400

    Edited my part of AntLabMain.java

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -72,7 +72,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Joshua  Perry");
-                  toPrint += p5.toString("gburdell5");
+                  toPrint += p5.toString("jperry45");
 
         System.out.println(toPrint);
 
